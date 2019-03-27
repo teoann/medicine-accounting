6d0579654f3d10fcfeac3ac875e85b882871ba7f
teoann <anngubarkova@gmail.com>
2019-03-27 11:03:05 +0200

    Creatiang table Products in the data base

---
+++ MedicineAccountingAPI/MedicineAccountingAPI/DataLevel/ContextMedicineAccounting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicineAccountingAPI.DataLevel
+{
+    public class ContextMedicineAccounting:DbContext
+    {
+        public ContextMedicineAccounting(DbContextOptions<ContextMedicineAccounting> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Product> Products { get; set; }
+        
+    }
+}
---
+++ MedicineAccountingAPI/MedicineAccountingAPI/DataLevel/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicineAccountingAPI.DataLevel
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+    }
+}
